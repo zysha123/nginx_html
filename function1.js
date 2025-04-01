dd6b7846209969d364f31de88a028ddfc52c4a26
410558186@qq.com <410558186@qq.com>
2025-04-01 16:03:33 +0800

    module

---
+++ function1.js
@@ -0,0 +1,15 @@
+function sum(x, y) {
+  return x + y;
+}
+
+function difference(x, y) {
+  return x - y;
+}
+
+function product(x, y) {
+  return x * y;
+}
+
+function quotient(x, y) {
+  return x / y;
+}
