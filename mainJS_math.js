5ff02ad140843c48b82976c0f946d67bbb69c2ca
410558186@qq.com <410558186@qq.com>
2025-04-06 14:22:01 +0800

    commonjs+es6

---
+++ mainJS_math.js
@@ -0,0 +1,22 @@
+function sum(x, y) {
+  return x + y;
+}
+
+function difference(x, y) {
+  return x - y;
+}
+
+function product(x, y) {
+  return x * y;
+}
+
+function quotient(x, y) {
+  return x / y;
+}
+
+module.exports = {
+  sum,
+  difference,
+  product,
+  quotient
+};
